21bac11ffcaae82a2e0c2a52d95013814f764ec1
shayan_mehranpoor <shayanmehranpoor@yahoo.com>
2021-04-23 18:41:48 +0430

    ui test added

--- GlobalTestUITests/GlobalTestUITests.swift
+++ GlobalTestUITests/GlobalTestUITests.swift
@@ -8,35 +8,27 @@
 import XCTest
 
 class GlobalTestUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testFetchContentButton() throws {
+                
+        let responseCodeLbl = app.staticTexts["Response Code: 32e02946-a160-4175-8280-c6561ffa6445"]
+        let counterLbl = app.staticTexts["Times Fetched: 15"]
+        let fetchContentBtn = app.buttons["Fetch Content"]
+        
+        XCTAssertTrue(responseCodeLbl.exists)
+        XCTAssertTrue(counterLbl.exists)
+        fetchContentBtn.tap()
+        XCTAssertTrue(responseCodeLbl.exists)
+        XCTAssertTrue(counterLbl.exists)
+
     }
 }
